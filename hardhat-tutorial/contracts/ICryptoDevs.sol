6f62839eee04e91b93dc3fe77a455f33f130ef8b
heysourin <sourin07@gmail.com>
2022-10-28 11:55:58 +0530

    Contract Updated

---
+++ hardhat-tutorial/contracts/ICryptoDevs.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.8;
+
+interface ICryptoDevs{
+    function tokenOfOwnerByIndex(address owner, uint index) external view returns(uint tokenId);//Uses mapping
+    
+    function balanceOf(address owner) external view returns (uint balance);
+}
