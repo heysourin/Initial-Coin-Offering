c709ee0fa9b3fff25d5b9e8f91cc5df4c518e7db
Sourin <98945276+heysourin@users.noreply.github.com>
2022-11-08 22:57:43 +0530

    Update ICryptoDevs.sol

--- hardhat-tutorial/contracts/ICryptoDevs.sol
+++ hardhat-tutorial/contracts/ICryptoDevs.sol
@@ -3,6 +3,5 @@ pragma solidity ^0.8.8;
 
 interface ICryptoDevs{
     function tokenOfOwnerByIndex(address owner, uint index) external view returns(uint tokenId);//Uses mapping
-    
     function balanceOf(address owner) external view returns (uint balance);
 }
